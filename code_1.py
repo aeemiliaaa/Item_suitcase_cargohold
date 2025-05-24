88e70447771a9fea705c92bf9328bcd53b3501f5
Mr Ma'am <amandananassy99@gmail.com>
2025-05-24 14:27:06 +0300

    Item class created -functionalities tested

--- code_1.py
+++ code_1.py
@@ -1,1 +1,25 @@
 # Write your solution here:
+class Item:
+    def __init__(self, name:str, weight:int):
+        self.__name = name 
+        self.__weight = weight
+
+    def name(self):
+        return self.__name
+    
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return f"{self.__name} ({self.__weight} kg)"
+
+
+if __name__ == "__main__":
+    book = Item("ABC Book", 2)
+    phone = Item("Nokia 3210", 1)
+
+    print("Name of the book:", book.name())
+    print("Weight of the book:", book.weight())
+
+    print("Book:", book)
+    print("Phone:", phone)
